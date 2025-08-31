3d750165c32d6c352f7014a5a2f0bf797353fa26
Heritage <okjohn644@gmail.com>
2025-08-31 19:09:04 +0100

    Updated website content

--- src/components/newuser.tsx
+++ src/components/newuser.tsx
@@ -22,7 +22,7 @@ export default function NewUserPage(){
         transition: "all 1s ease-in-out"
       }}>
 
-        <h2 className="welcomeback">welcome, we're happy to have you again {":)"} </h2>
+        <h2 className="welcomeback">Welcome, we're happy to have you again {":)"} </h2>
 <button className="buttonforwelcomeback"  onClick={() => {
              setTimeout(() =>  navigate("/home"), 1500);
             }} >Continue to app</button>
